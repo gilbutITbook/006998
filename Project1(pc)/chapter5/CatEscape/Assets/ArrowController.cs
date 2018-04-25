cee537d0c88c3a139a4c3e5c976559cddc3b7c98
gilbut <gilbutitbook@gmail.com>
2018-04-26 08:43:56 +0900

    initial release

---
+++ Project1(pc)/chapter5/CatEscape/Assets/ArrowController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+	GameObject player;
+
+	void Start() {
+		this.player = GameObject.Find("player");
+	}
+
+	void Update() {
+		transform.Translate(0, -0.1f, 0);
+		if(transform.position.y < -5.0f) {
+			Destroy(gameObject);
+		}
+
+		// 충돌 판정
+		Vector2 p1 = transform.position; // 화살의 중심 좌표
+		Vector2 p2 = this.player.transform.position; // 플레이어의 중심 좌표
+		Vector2 dir = p1 - p2;
+		float d = dir.magnitude;
+		float r1 = 0.5f; // 화살 반경
+		float r2 = 1.0f; // 플레이어 반경
+
+		if(d < r1 + r2) {
+			// 감독 스크립트에 플레이어와 화살이 충돌했다고 전달한다
+			GameObject director = GameObject.Find("GameDirector");
+			director.GetComponent<GameDirector>().DecreaseHp();
+
+			// 충돌하면 화살을 소멸시킨다
+			Destroy(gameObject);
+		}
+	}
+}
